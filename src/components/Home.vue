fad9e368825c8efc2dc8c3543d3c3e96589ae597
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-06-29 15:29:44 +0200

    Add home temporary file

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <button>
+      <router-link to="/primapagina">Prima pagina</router-link>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
