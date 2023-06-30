b5d67cc6b3daa7ae1a65273a550d47c32217a619
Enrico Lumini <enrico.lumini@studio.unibo.it>
2023-06-30 11:17:49 +0200

    Add check not to overwrite db data already present

--- docker/save_db.sh
+++ docker/save_db.sh
@@ -4,6 +4,24 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 OUTDIR="${SCRIPT_DIR}/db-data"
 DBNAME="aws-project-db"
 
+if [ -d "${OUTDIR}" ] && [ "$(ls -A ${OUTDIR})" ]; then
+    echo "Output directory is not empty, found data inside:"
+    if command -v tree &> /dev/null; then
+      tree "${OUTDIR}"
+    else
+      ls "${OUTDIR}"
+    fi
+    while true; do
+      read -p "Would you like to overwrite data?[Yes/No]" yn
+      case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "No data has been modified"; exit;;
+        * ) echo "Please answer yer or no.";;
+      esac
+    done
+
+fi
+
 mongodump -d "${DBNAME}" -o "${OUTDIR}"
 
 
