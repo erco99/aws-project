0023a5a3bfd01c0e4354549be8223e622d8d5ff6
Enrico Lumini <enrico.lumini@studio.unibo.it>
2023-06-30 14:01:54 +0200

    Fix compatibility problem with docker compose command in stop script

--- docker/stop_db.sh
+++ docker/stop_db.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker compose -f docker-compose-mongo-db.yml --env-file=config.env down --remove-orphans
+
+if command -v docker-compose &> /dev/null; then
+  docker-compose -f docker-compose-mongo-db.yml --env-file=config.env down --remove-orphans
+else
+  docker compose -f docker-compose-mongo-db.yml --env-file=config.env down --remove-orphans
+fi
