28098de16ddc84ef95318701dd7fef89e911f16f
Enrico Lumini <enrico.lumini@studio.unibo.it>
2023-06-30 13:52:17 +0200

    Fix compatibility problem with docker compose command

--- docker/start_db.sh
+++ docker/start_db.sh
@@ -7,5 +7,9 @@ if [ -f config.env ]; then
   export $(cat config.env | sed 's/#.*//g' | xargs)
 fi
 
-docker-compose -f docker-compose-mongo-db.yml --env-file=config.env up -d --remove-orphans
+if command -v docker-compose &> /dev/null; then
+    docker-compose -f docker-compose-mongo-db.yml --env-file=config.env up -d --remove-orphans
+else
+    docker compose -f docker-compose-mongo-db.yml --env-file=config.env up -d --remove-orphans
+fi
 ${SCRIPT_DIR}/utils/wait-for-it.sh ${MONGO_URL}:${MONGO_PORT} --strict --timeout=10 -- echo "MongoDB is up"
