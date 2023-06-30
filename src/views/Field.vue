ec642202473e9b0dcef18404e46c7264bd9b7f94
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-06-30 16:42:31 +0200

    Add a view for field

---
+++ src/views/Field.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex bg-success">
+    <div class="border border-dark p-2 flex-grow-1">09:00</div>
+    <div class="border border-dark p-2 flex-grow-1">10:00</div>
+    <div class="border border-dark p-2 flex-grow-1">11:00</div>
+    <div class="border border-dark p-2 flex-grow-1">12:00</div>
+    <div class="border border-dark p-2 flex-grow-1">13:00</div>
+    <div class="border border-dark p-2 flex-grow-1">14:00</div>
+    <div class="border border-dark p-2 flex-grow-1">15:00</div>
+    <div class="border border-dark p-2 flex-grow-1">16:00</div>
+    <div class="border border-dark p-2 flex-grow-1">17:00</div>
+    <div class="border border-dark p-2 flex-grow-1">18:00</div>
+    <div class="border border-dark p-2 flex-grow-1">19:00</div>
+    <div class="border border-dark p-2 flex-grow-1">20:00</div>
+    <div class="border border-dark p-2 flex-grow-1">21:00</div>
+    <div class="border border-dark p-2 flex-grow-1">22:00</div>
+  </div>
+</template>
