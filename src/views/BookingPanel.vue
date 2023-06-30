98ea6527c205b05a94bca6926fd9d59c3f0e6e04
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-06-30 18:20:16 +0200

    New View for the booking panel

---
+++ src/views/BookingPanel.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Field from "./Field.vue";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: [
+        "Campo 1",
+        "Campo 2",
+        "Campo 3",
+        "Campo 4",
+        "Campo 5",
+        "Campo 6",
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container-fluid overflow-x-auto">
+    <div class="py-2" v-for="field in fields">
+      <h4>{{ field }}</h4>
+      <Field />
+    </div>
+  </div>
+</template>
--- src/views/Field.vue
+++ src/views/Field.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="d-flex bg-success">
-    <div class="border border-dark p-2 flex-grow-1">09:00</div>
-    <div class="border border-dark p-2 flex-grow-1">10:00</div>
-    <div class="border border-dark p-2 flex-grow-1">11:00</div>
-    <div class="border border-dark p-2 flex-grow-1">12:00</div>
-    <div class="border border-dark p-2 flex-grow-1">13:00</div>
-    <div class="border border-dark p-2 flex-grow-1">14:00</div>
-    <div class="border border-dark p-2 flex-grow-1">15:00</div>
-    <div class="border border-dark p-2 flex-grow-1">16:00</div>
-    <div class="border border-dark p-2 flex-grow-1">17:00</div>
-    <div class="border border-dark p-2 flex-grow-1">18:00</div>
-    <div class="border border-dark p-2 flex-grow-1">19:00</div>
-    <div class="border border-dark p-2 flex-grow-1">20:00</div>
-    <div class="border border-dark p-2 flex-grow-1">21:00</div>
-    <div class="border border-dark p-2 flex-grow-1">22:00</div>
+  <div class="d-flex">
+    <div class="border border-dark p-2 flex-grow-1 bg-success">09:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">10:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">11:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">12:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">13:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">14:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">15:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">16:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">17:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">18:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">19:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">20:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">21:00</div>
+    <div class="border border-dark p-2 flex-grow-1 bg-success">22:00</div>
   </div>
 </template>
