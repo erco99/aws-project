a4a9b54998c8d4ce5ac81f726615d9bd6a743e32
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-07-03 11:11:22 +0200

    Remove samples

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import BookingPanel from "../views/BookingPanel.vue";
 import Login from "../views/login/Login.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
     {
       path: "/booking-panel",
       name: "booking-panel",
@@ -23,7 +17,7 @@ const router = createRouter({
       path: "/login",
       name: "login",
       component: Login,
-    }
+    },
   ],
 });
 
