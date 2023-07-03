64ccb8eb220eb0c52fe3bd5b5d35a48a772f1045
Francesco Ercolani <erco99@live.it>
2023-07-03 16:47:08 +0200

    Add index file of layout

---
+++ src/layout/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-layout class="rounded rounded-md">
+        <v-app-bar title="Application bar"></v-app-bar>
+
+        <v-navigation-drawer>
+        <v-list>
+            <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+        </v-navigation-drawer>
+
+        <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+            <app-main/>
+        </v-main>
+    </v-layout>
+</template>
+
+<script>
+import AppMain from './components/AppMain.vue'
+
+export default {
+    name: "Layout",
+    components: {
+        AppMain
+    }
+}
+</script>
