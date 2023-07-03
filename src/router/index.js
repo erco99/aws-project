b57cb69e3a81c79bb4cbd6074e26d801a99a35c0
Francesco Ercolani <erco99@live.it>
2023-07-03 16:49:00 +0200

    Change router index file for layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Layout from "../layout/Index.vue";
 import BookingPanel from "../views/BookingPanel.vue";
 import Login from "../views/login/Login.vue";
 
@@ -6,12 +7,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/booking-panel",
-      name: "booking-panel",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: BookingPanel,
+      path: "/",
+      component: Layout,
+      redirect: "/booking-panel",
+      children: [
+        {
+          path: "/booking-panel",
+          name: "booking-panel",
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: BookingPanel,
+        }
+      ]
     },
     {
       path: "/login",
