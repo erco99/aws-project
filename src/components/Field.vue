1b329712f58b4ec7f98bdf5becf1b1646b5ff34d
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-07-03 12:11:22 +0200

    Add props and Dialog to Field.vue

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,27 +1,44 @@
 <template>
   <h4>{{ name }}</h4>
   <div class="d-flex">
-    <div class="border border-dark p-2 flex-grow-1 bg-success">09:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">10:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">11:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">12:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">13:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">14:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">15:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">16:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">17:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">18:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">19:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">20:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">21:00</div>
-    <div class="border border-dark p-2 flex-grow-1 bg-success">22:00</div>
+    <div
+      role="button"
+      class="border border-dark p-2 bg-success"
+      v-for="hour in hours"
+      @click="book(hour)">
+      {{ hour }}
+    </div>
   </div>
+  <v-dialog v-model="dialog" scrollable width="auto"
+    ><v-card>
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" block @click="dialog = false">Prenota ora</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
   props: {
     name: String,
+    hours: [String],
+  },
+  data() {
+    return {
+      bookingHour: "",
+      dialog: false,
+    };
+  },
+  methods: {
+    book(hour) {
+      this.bookingHour = hour;
+      this.dialog = true;
+    },
   },
 };
 </script>
