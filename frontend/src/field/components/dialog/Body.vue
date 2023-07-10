a26501cbae847d14e60bcc3d335331d1a466e343
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-07-10 17:09:35 +0200

    Refactor props Est Body

--- frontend/src/field/components/dialog/Body.vue
+++ frontend/src/field/components/dialog/Body.vue
@@ -3,10 +3,31 @@ import East from "./body/Est.vue";
 import Middle from "./body/Middle.vue";
 import West from "./body/West.vue";
 </script>
+
+<script>
+export default {
+  emits: ["durationUpdate", "matchUpdate"],
+  props: {
+    next: Boolean,
+  },
+  methods: {
+    notifyDuration(value) {
+      this.$emit("durationUpdate", value);
+    },
+    notifyMatch(value) {
+      this.$emit("matchUpdate", value);
+    },
+  },
+};
+</script>
+
 <template>
   <v-row>
     <v-col cols="12" sm="4">
-      <East></East>
+      <East
+        :next="next"
+        @duration-update="notifyDuration"
+        @match-update="notifyMatch"></East>
     </v-col>
     <v-col cols="12" sm="4">
       <Middle></Middle>
