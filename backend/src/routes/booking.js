2020233e946106922181a1e2ae84b624132b14ba
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-07-17 18:41:05 +0200

    add week route for booking

---
+++ backend/src/routes/booking.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const bookingController = require("../controllers/bookingController");
+
+router.post("/week", bookingController.getWeek);
