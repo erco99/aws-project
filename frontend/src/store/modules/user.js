315bcc91cb162240f5a8df49950a5f267b27e3b8
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-07-18 13:39:49 +0200

    Add token to store and login action

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -1,16 +1,35 @@
+import { login } from '@/api/user'
+
 const state = {
   name: 'PROVANOME',
+  token: ''
 }
 
 const mutations = {
   SET_NAME: (state, name) => {
     state.name = name
   },
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  }
 }
 
 const actions = {
     getName( {commit} ) {
         commit('SET_NAME', 'prova')
+    },
+
+    login({ commit }, loginData) {
+      return new Promise((resolve, reject) => {
+        login(loginData).then(response => {
+          const { data } = response
+          commit('SET_TOKEN', data.access_token)
+          setToken(data.token)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
     }
 }
 
