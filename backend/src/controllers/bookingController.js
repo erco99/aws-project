624ec8b0f193fe1741ccd5100f8890d7c53d909d
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-07-18 12:01:36 +0200

    Implement week handler without errors

--- backend/src/controllers/bookingController.js
+++ backend/src/controllers/bookingController.js
@@ -1,14 +1,22 @@
 const bookings = require("../models/bookings");
 
 async function getWeek(req, res) {
-  const { from } = req.body;
-  const to = new Date().setDate(from.getDate() + (7 - from.getDay()));
-  try {
-    const week = await bookings.find({ day: { $gte: from, $lt: to } });
-    res.json(week);
-  } catch (e) {
-    res.status(500);
-  }
+  // Iitialization
+  const { day } = req.body;
+  const from = new Date(day);
+  const to = new Date();
+
+  // Set date to nearest sunday
+  to.setUTCDate(from.getDate() + (7 - from.getDay()));
+
+  // Set time to 23:59
+  to.setUTCHours(23, 59, 59, 0);
+
+  // Query
+  const week = await bookings.find({ day: { $gte: from, $lte: to } });
+
+  // Response
+  res.json(week);
 }
 
 module.exports = { getWeek };
