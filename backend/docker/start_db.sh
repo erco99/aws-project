dbf1640af5df67f01b59ecb28c692fcd6800f4ce
Francesco Ercolani <erco99@live.it>
2023-07-18 14:49:49 +0200

    Solve avx support problem in docker compose mongo version

--- backend/docker/start_db.sh
+++ backend/docker/start_db.sh
@@ -2,14 +2,25 @@
 # set -exo
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+MONGO_VERSION="latest"
 
 if [ -f config.env ]; then
   export $(cat config.env | sed 's/#.*//g' | xargs)
 fi
 
-if command -v docker-compose &> /dev/null; then
-    docker-compose -f docker-compose-mongo-db.yml --env-file=config.env up -d --remove-orphans
+if [[ -z `cat /proc/cpuinfo | grep avx` ]]; then
+  if command -v docker-compose &> /dev/null; then
+    docker-compose -f docker-compose-mongo-db-noavx.yml --env-file=config.env up -d --remove-orphans
+  else
+      docker compose -f docker-compose-mongo-db-noavx.yml --env-file=config.env up -d --remove-orphans
+  fi
 else
-    docker compose -f docker-compose-mongo-db.yml --env-file=config.env up -d --remove-orphans
+  if command -v docker-compose &> /dev/null; then
+    docker-compose -f docker-compose-mongo-db.yml --env-file=config.env up -d --remove-orphans
+  else
+      docker compose -f docker-compose-mongo-db.yml --env-file=config.env up -d --remove-orphans
+  fi
 fi
+
+
 ${SCRIPT_DIR}/utils/wait-for-it.sh ${MONGO_URL}:${MONGO_PORT} --strict --timeout=10 -- echo "MongoDB is up"
