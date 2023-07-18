e907be56136a8bfd4721b53aa5f9c799b6eec8b5
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2023-07-18 10:32:32 +0200

    Add export

--- backend/src/routes/booking.js
+++ backend/src/routes/booking.js
@@ -3,3 +3,5 @@ const router = express.Router();
 const bookingController = require("../controllers/bookingController");
 
 router.post("/week", bookingController.getWeek);
+
+module.exports = router;
