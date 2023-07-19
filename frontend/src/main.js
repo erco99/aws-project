24e74ef0ecaaf7e829b50cabd6b971c235fd2af6
Francesco Ercolani <erco99@live.it>
2023-07-19 13:56:49 +0200

    Add permissions.js file for routing guard

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import store from './store'
 import router from "./router";
 import mitt from 'mitt';
 
+import './permission'
+
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
---
+++ frontend/src/permission.js
@@ -0,0 +1,28 @@
+import router from './router/index'
+import store from './store/index'
+import { getToken } from './utils/authentication';
+
+const whiteList = ["/login"];
+
+router.beforeEach(async(to, from, next) => {
+
+    const token = getToken()
+
+    if(token) {
+        if (to.path === '/login') {
+            // if is logged in, redirect to the home page
+            next({ path: '/' })
+            NProgress.done()
+          } else {
+                console.log(token)
+                next()
+          }
+    } else {
+        if (whiteList.indexOf(to.path) !== -1) {
+            next();
+        } else {
+            next(`/login`);
+            NProgress.done();
+        }
+    }
+})
