98abe019d344f1a69eadc6c808983824fdb9a611
Enrico Lumini <enrico.lumini@studio.unibo.it>
2023-07-27 17:44:39 +0200

    Fix backend user API return data format

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,8 +1,8 @@
 const User = require("../models/user");
 
 async function users(req, res) {
-  const users = await User.find({}, { name: 1, surname: 1, _id: 1 }).exec();
-  return res.status(200).json({ users });
+  const users = await User.find({}, { name: 1, surname: 1, email: 1, _id: 0 }).exec();
+  return res.status(200).json(users);
 }
 
 module.exports = { users };
