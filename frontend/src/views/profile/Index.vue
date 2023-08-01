7bf773a41deaa1e5dd21cf8d651f42f0981239ed
Francesco Ercolani <erco99@live.it>
2023-08-01 17:55:51 +0200

    Create profile component skeleton

--- frontend/src/views/profile/Index.vue
+++ frontend/src/views/profile/Index.vue
@@ -1,25 +1,33 @@
 <template>
-  <v-row>
-    <v-col sm="12" md="4">
-      <user />
+  <v-row >
+    <v-col sm="12" md="4" class="align-self-baseline">
+        <user />
+        <timeline  style="margin-top: 15px;"/>
     </v-col>
 
     <v-col sm="12" md="8">
-      <activity />
+      <activity/>
     </v-col>
 
+  </v-row>
+  <v-row>
+    <v-col md="4" class="align-self-baseline">
+   
+    </v-col>
   </v-row>
 </template>
 
 <script>
 import User from "./components/User.vue"
 import Activity from "./components/Activity.vue"
+import Timeline from "./components/Timeline.vue"
 
 export default {
   name: "Profile",
   components: {
     User,
-    Activity
+    Activity,
+    Timeline
   }
   
 }
--- frontend/src/views/profile/components/Activity.vue
+++ frontend/src/views/profile/components/Activity.vue
@@ -8,7 +8,32 @@
         <div class="text-h6 mb-1">
           Headline
         </div>
-        <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
+        <div class="text-caption">Sed ut perspiciat
+          is unde omnis iste natus error sit voluptatem accus
+          antium doloremque laudantium, totam rem aperiam eaque ip
+          sa, quae ab illo inventore veritatis et quasi architecto be
+          atae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia volu
+          ptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni do
+          lores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam
+           est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci ve
+           lit, sed quia non numquam eius modi tempora incidunt, ut labore et dol
+           ore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis n
+           ostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid e
+           x ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea vo
+           luptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum
+            fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio 
+            dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti
+            , quos dolores et quas molestias excepturi sint, obcaecati cupiditate non providen
+            t, similique sunt in culpa, qui officia deserunt mollitia animi, id est labo
+            rum et dolorum fuga. Et harum quidem rerum facilis est et expedita distincti
+            o. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impe
+            dit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda es
+            t, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis 
+            aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae
+             non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+              voluptat
+          ibus maiores alias consequatur aut perferendis doloribus asperiores repe
+          llat.»</div>
       </div>
     </v-card-item>
 
