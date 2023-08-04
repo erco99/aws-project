b4788d00aa0c677ac7c44cc7a8513e5c6a0530c0
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-08-04 17:39:19 +0200

    Add credits backend controller

---
+++ backend/src/controllers/creditsController.js
@@ -0,0 +1,19 @@
+const User = require("../models/user");
+
+async function paymentMethodInsert(req, res) {
+  //qui ci andrebbe il controllo per verificare che la carta
+  //è una carta reale ma noi lo saltiamo in quanto i pagamenti
+  //sono ovviamente simulati
+  const payment_method = req.body;
+
+  console.log(payment_method)
+
+  try {
+    await User.create({payment_method});
+  } catch (error) {
+      console.log(error);
+      return res.status(500).json({'message': 'Error'});
+  }
+}
+
+module.exports = {paymentMethodInsert}
