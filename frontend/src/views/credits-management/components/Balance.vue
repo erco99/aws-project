36b87b8bf8dcb7a1880af52f9d09d29b1acec0e1
Francesco Ercolani <erco99@live.it>
2023-08-08 17:33:00 +0200

    Add deposit withdraw and send form

--- frontend/src/views/credits-management/components/Balance.vue
+++ frontend/src/views/credits-management/components/Balance.vue
@@ -16,7 +16,7 @@
     </v-card-item>  
     <v-card-item v-else>
       <v-btn
-        @click="dialog = true; operationType='deposit'"     
+        @click="dialog = true; operationType='deposito'"     
         color="green-darken-4"
         style="margin-right: 5px"
         width="150"
@@ -24,14 +24,14 @@
         Deposita
       </v-btn>
       <v-btn 
-        @click="dialog = true; operationType='withdraw'"
+        @click="dialog = true; operationType='ritiro'"
         color="red-darken-4" 
         style="margin-right: 5px" 
         width="150">
         Ritira
       </v-btn>
       <v-btn 
-        @click="dialog = true; operationType='send'"
+        @click="dialog = true; operationType='invio'"
         color="yellow-darken-4" 
         width="150"> 
         Invia </v-btn>
@@ -42,7 +42,45 @@
           v-model="dialog"
           width="500">
       <v-card>
-        {{operationType}}
+        <v-card-item>
+              <div class="text-overline mb-1">
+                {{ operationType }} denaro
+              </div>
+            </v-card-item>
+            <v-divider></v-divider>
+            <v-card-text class="pa-5">
+              <v-form ref="form">
+                <div class="currency-input-container">
+                  <div class="font-weight-medium label-div" 
+                    style="margin-bottom: 10px;">
+                    Importo
+                  </div>
+                  <v-text-field 
+                    variant="outlined"
+                    prepend-inner-icon="mdi-currency-eur"
+                    type="text" 
+                    id="currency-input" 
+                    v-model="amountValue"
+                    v-on:keypress="isNumber($event, amountValue)">
+                  </v-text-field>
+                </div>
+                <v-btn
+                    outlined 
+                    color="green"
+                    block
+                    class="me-4"
+                    text="Conferma"
+                    style="margin-bottom: 10px"
+                  ></v-btn>
+                  <v-btn
+                    outlined
+                    color="red"
+                    block
+                    @click="cancel"
+                    text="Annulla"
+                  ></v-btn>
+              </v-form>
+            </v-card-text>
       </v-card>
      </v-dialog>
   </v-card>
@@ -59,10 +97,22 @@ export default {
     sendCredit() {
       console.log(this.$store.getters.userPaymentMethod);
     },
+    cancel() {
+      this.dialog = false
+      this.$refs.form.reset()
+    },
+    isNumber(event, value) {
+      if (!/\d/.test(event.key) &&  (event.key !== "," || /\./.test(value))) {
+        return event.preventDefault(); 
+      }
+
+      if (/\,\d{2}/.test(value)) return event.preventDefault(); 
+    },
   },
   data: () => ({
     dialog: false,
-    operationType: ''
+    operationType: '',
+    amountValue: ''
   })
 };
 </script>
