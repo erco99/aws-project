c9241e59e35b1fb0e38602a3df4cd80e231da88e
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-08-08 14:15:43 +0200

    Complete transactions schema

--- backend/src/models/transactions.js
+++ backend/src/models/transactions.js
@@ -3,15 +3,30 @@ const mongoose = require("mongoose");
 const transactionsSchema = mongoose.Schema({
     _id: true,
     amount: {
-        type: String,
+        type: Number,
         required: true
     },
     date: {
         type: String,
         required: true
     },
+    time: {
+        type: String,
+        required: true
+    },
     user: {
-        
+        name: {
+            type: String,
+            required: true
+        },
+        surname: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        }
     }
 });
   
