f8ddd19e73fe723481f811c0547fae315f47f03b
Francesco Ercolani <erco99@live.it>
2023-08-10 18:08:22 +0200

    Create store file for transactions

---
+++ frontend/src/store/modules/transactions.js
@@ -0,0 +1,34 @@
+import { getTransactions } from '@/api/credits'
+
+const state = {
+  transactions: ''
+}
+
+const mutations = {
+  SET_TRANSACTIONS: (state, data) => {
+    state.transactions = data
+  }
+}
+
+const actions = {
+  getTransactions({commit}, email) {
+    return new Promise((resolve, reject) => {
+      getTransactions(email).then(response => {
+        const { data } = response
+        commit('SET_TRANSACTIONS', data)
+        console.log(state.transactions)
+
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
