76a3ad1d8e5f0dff24c21bbc1abb6dcd0b2427e4
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-08-10 13:54:09 +0200

    Add table for transactions

--- frontend/src/views/credits-management/components/Transactions.vue
+++ frontend/src/views/credits-management/components/Transactions.vue
@@ -1,21 +1,82 @@
 <template>
     <v-card class="mx-auto rounded-0" elevation="4">
       <v-card-item>
-        <div>
           <div class="text-overline mb-1">
-            FSDFD
+            Transazioni
           </div>
-          <div class="text-h6 mb-1">
-            Headline
-          </div>
-          <div class="text-caption">Greyhound divisely hello coldly fonwderfully</div>
-        </div>
+            <v-table>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  Name
+                </th>
+                <th class="text-left">
+                  Calories
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in desserts"
+                :key="item.name"
+              >
+                <td>{{ item.name }}</td>
+                <td>{{ item.calories }}</td>
+              </tr>
+            </tbody>
+              </v-table>
       </v-card-item>
-  
-      <v-card-actions>
-        <v-btn variant="outlined">
-          Button
-        </v-btn>
-      </v-card-actions>
     </v-card>
 </template>
+
+<script>
+export default {
+  data: () => {
+     return {
+desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+          },
+        ],
+      }
+
+  }
+}
+</script>
