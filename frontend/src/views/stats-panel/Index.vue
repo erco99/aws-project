89bc7528175eb8e3f0d53b5582bc89edd3690807
Enrico Lumini <enrico.lumini@studio.unibo.it>
2023-08-12 21:50:35 +0200

    Add scheleton for stats graph

--- frontend/src/views/stats-panel/Index.vue
+++ frontend/src/views/stats-panel/Index.vue
@@ -1,3 +1,23 @@
 <template>
+  <v-row >
+    <v-col sm="12" md="4" class="align-self-baseline">
+      <BookingsGraph></BookingsGraph>
+    </v-col>
 
+    <v-col sm="12" md="8">
+
+    </v-col>
+
+  </v-row>
+  <v-row>
+    <v-col md="4" class="align-self-baseline">
+    </v-col>
+  </v-row>
 </template>
+
+<script>
+import BookingsGraph from "@/views/stats-panel/components/bookings-graph.vue";
+export default {
+    components: { BookingsGraph }
+}
+</script>
---
+++ frontend/src/views/stats-panel/components/bookings-graph.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="mx-auto rounded-0" elevation="4">
+    <Bar
+        id="my-chart-id"
+        :options="chartOptions"
+        :data="chartData"
+    />
+  </v-card>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [{
+          label: "Prenotazioni",
+          backgroundColor: '#467a3b',
+          data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script>
+
