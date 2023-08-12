2c00c16e5b727a24b9febf4480af3b977b3606c8
Enrico Lumini <enrico.lumini@studio.unibo.it>
2023-08-12 18:51:45 +0200

    Add background image to login form

---
+++ frontend/src/components/bg-image/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="page-signin-bg">
+    <div class="background-image"></div>
+    <div class="overlay"></div>
+  </div>
+</template>
+
+<style>
+#page-signin-bg {
+  height: 100%;
+  left: 0;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.background-image {
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  background-attachment: fixed;
+  background-image: url('bg.jpg')
+}
+
+.overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  background: -webkit-linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.2),rgba(0,0,0,.5));
+}
+</style>
--- frontend/src/views/login/Index.vue
+++ frontend/src/views/login/Index.vue
@@ -1,8 +1,11 @@
 <script setup>
   import LoginCard from "@/views/login/components/LoginCard.vue";
+  import Body from "@/views/booking-panel/components/dialog/Body.vue";
+  import BgImage from "@/components/bg-image/Index.vue";
 </script>
 
 <template>
+  <BgImage></BgImage>
   <v-container class="fill-height">
     <v-row justify="center" align="center">
       <v-col>
