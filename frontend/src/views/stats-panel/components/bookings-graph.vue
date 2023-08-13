b16892dc8d5a9f34f7a590231a3486d5c85d7e07
Enrico Lumini <enrico.lumini@studio.unibo.it>
2023-08-13 16:42:07 +0200

    Add some style to line chart

--- frontend/src/views/stats-panel/components/bookings-graph.vue
+++ frontend/src/views/stats-panel/components/bookings-graph.vue
@@ -1,20 +1,22 @@
 <template>
-  <v-card class="mx-auto rounded-0" elevation="4">
+  <v-card class="mx-auto rounded-4" elevation="4">
+    <v-card-item>
     <v-chart class="chart" :option="option" autoresize></v-chart>
+    </v-card-item>
   </v-card>
 </template>
 
 <script>
-import { use } from "echarts/core"
+import  * as echarts from "echarts/core"
 import { CanvasRenderer } from "echarts/renderers";
 import { LineChart } from "echarts/charts";
 import { GridComponent } from 'echarts/components';
 import { TitleComponent, TooltipComponent, LegendComponent } from "echarts/components";
 import VChart, { THEME_KEY } from "vue-echarts";
-import { provide } from "vue";
+
 import PageNumber from "@/views/signup/components/commons/PageNumber.vue";
 
-use([ CanvasRenderer, LineChart, TitleComponent, TooltipComponent, LegendComponent, GridComponent ])
+echarts.use([ CanvasRenderer, LineChart, TitleComponent, TooltipComponent, LegendComponent, GridComponent ])
 
 export default {
   components: {PageNumber, VChart },
@@ -25,26 +27,74 @@ export default {
           text: "Prenotazioni per mese",
           left: "center"
         },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            label: {
+              backgroundColor: "#6a7985"
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
         xAxis: {
           type: 'category',
-          data: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile'],
+          boundaryGap: false,
+          axisTick: {
+            alignWithLabel: true,
+          },
+          axisLabel: {
+            rotate: 40
+          },
+          data: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
         },
         yAxis: {
           type: 'value',
         },
         series: [
             {
+              name: "Prenotazioni",
               type: 'line',
-              data: [ 20, 30, 70, 10],
+              smooth: true,
+              lineStyle: {
+                width: 0
+              },
+              showSymbol: false,
+              areaStyle: {
+                opacity: 0.8,
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: 'rgb(197, 232, 183)'
+                  },
+                  {
+                    offset: 0.2,
+                    color: 'rgb(171, 224, 152)'
+                  },
+                  {
+                    offset: 0.4,
+                    color: 'rgb(131, 212, 117)'
+                  },
+                  {
+                    offset: 0.6,
+                    color: 'rgb(87, 200, 77)'
+                  },
+                  {
+                    offset: 1,
+                    color: 'rgb(46, 182, 44)'
+                  }
+                ])
+              },
+              emphasis: {
+                focus: 'series'
+              },
+              data: [ 20, 30, 70, 10, 0, 0, 10, 20, 30, 20, 60, 20 ]
           }
         ],
-        emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)',
-            }
-        }
       }
     }
   }
