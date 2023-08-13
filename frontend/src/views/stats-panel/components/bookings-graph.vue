2b32dd98559ae03988f660c00c5aa7461077f052
Enrico Lumini <enrico.lumini@studio.unibo.it>
2023-08-13 15:56:18 +0200

    Move from vue-chartjs to echarts

--- frontend/src/views/stats-panel/components/bookings-graph.vue
+++ frontend/src/views/stats-panel/components/bookings-graph.vue
@@ -1,35 +1,59 @@
 <template>
   <v-card class="mx-auto rounded-0" elevation="4">
-    <Line
-        :options="chartOptions"
-        :data="chartData"
-    />
+    <v-chart class="chart" :option="option" autoresize></v-chart>
   </v-card>
 </template>
 
 <script>
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
-import { Line } from 'vue-chartjs'
+import { use } from "echarts/core"
+import { CanvasRenderer } from "echarts/renderers";
+import { LineChart } from "echarts/charts";
+import { GridComponent } from 'echarts/components';
+import { TitleComponent, TooltipComponent, LegendComponent } from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { provide } from "vue";
+import PageNumber from "@/views/signup/components/commons/PageNumber.vue";
 
-ChartJS.register( CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend )
+use([ CanvasRenderer, LineChart, TitleComponent, TooltipComponent, LegendComponent, GridComponent ])
 
 export default {
-  components: { Line },
+  components: {PageNumber, VChart },
   data() {
     return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [{
-          label: "Prenotazioni",
-          backgroundColor: '#467a3b',
-          data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false
+      option: {
+        title: {
+          text: "Prenotazioni per mese",
+          left: "center"
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile'],
+        },
+        yAxis: {
+          type: 'value',
+        },
+        series: [
+            {
+              type: 'line',
+              data: [ 20, 30, 70, 10],
+          }
+        ],
+        emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)',
+            }
+        }
       }
     }
   }
 }
 </script>
+
+<style scoped>
+  .chart {
+    height: 30vh;
+  }
+</style>
 
