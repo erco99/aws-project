deaf3ba99f04518b089514356518c276f8445462
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-08-16 10:29:25 +0200

    Add modify payment method feature

--- frontend/src/views/credits-management/components/PayMethod.vue
+++ frontend/src/views/credits-management/components/PayMethod.vue
@@ -1,173 +1,175 @@
 <template>
-    <v-card class="mx-auto rounded-0" elevation="4">
-      <v-card-item>
+  <v-card class="mx-auto rounded-0" elevation="4">
+    <v-card-item>
+        <div class="text-overline mb-1">
+          Metodo di pagamento
+        </div>
+    </v-card-item>
+
+    <v-dialog
+      persistent
+      v-model="dialog"
+      width="500">
+      <v-card>
+        <v-card-item>
           <div class="text-overline mb-1">
-            Metodo di pagamento
+            Inserimento metodo di pagamento
           </div>
+        </v-card-item>
+        <v-divider></v-divider>
+        <v-card-text class="pa-5">
+          <v-form ref="form">
+            <div class="font-weight-medium label-div">Numero carta</div>
+              <v-text-field 
+                placeholder="xxxx-xxxx-xxxx-xxxx"
+                density="compact"
+                :rules="cardNumberRules"
+                :value="formatCardNumber"
+                @input="updateValue"
+                v-model="valueFields.cardNumber" 
+                @keypress="isNumber($event)"
+                maxlength="19"
+                variant="outlined">
+              </v-text-field>
+              <div class="font-weight-medium label-div">Titolare carta</div>
+              <v-text-field 
+                placeholder="es. Mario Rossi"
+                density="compact"
+                :rules="cardNameRules"
+                v-model="valueFields.cardName"
+                variant="outlined">
+              </v-text-field>
+              <v-row style="margin-bottom: 5px">
+                <v-col cols="8">
+                  <div class="font-weight-medium label-div">Scadenza carta</div>
+                  <div style="display: flex">
+                    <v-select
+                      label="Mese"
+                      style="margin-right: 15px"
+                      density="compact"
+                      variant="outlined"
+                      v-model="valueFields.cardMonth"
+                      :items="months"
+                      :rules="dateYearRules"></v-select>
+                    <v-select 
+                      label="Anno"
+                      density="compact"
+                      variant="outlined"
+                      v-model="valueFields.cardYear"
+                      :items="years"
+                      :rules="dateYearRules"></v-select>
+                  </div>
+                </v-col>
+                <v-col>
+                  <div class="font-weight-medium label-div">CVV/CVC</div>
+                  <v-text-field 
+                    @keypress="isNumber($event)"
+                    maxlength="3"
+                    placeholder="xxx"
+                    density="compact"
+                    variant="outlined"
+                    v-model="valueFields.cardCvv"
+                    :rules="cardCvvRules"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-btn
+                outlined 
+                color="green"
+                block
+                class="me-4"
+                text="Conferma"
+                @click="validate"
+                style="margin-bottom: 10px"
+              ></v-btn>
+              <v-btn
+                outlined
+                color="red"
+                block
+                @click="cancelPayInsert"
+                text="Annulla"
+              ></v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <div v-if="userPaymentMethod==null">
+      <v-card-item>
+        Nessun metodo di pagamento registrato
       </v-card-item>
 
-      <div v-if="userPaymentMethod==null">
-        <v-card-item>
-          Nessun metodo di pagamento registrato
-        </v-card-item>
+      <v-card-item>
+        <v-btn          
+          color="yellow-darken-4" 
+          @click="dialog = true; ">
+          Aggiungi
+        </v-btn>
+      </v-card-item>
+    </div>
+    
+    <div class="paycard" v-else>
+      <v-card-item>
+      <vue-paycard :value-fields="valueFields"
+          :has-random-backgrounds="false"
+          style="margin-bottom:15px"/>
+      </v-card-item>
+      <v-card-item>
+        <v-btn
+          outlined
+          width="150"
+          style="margin-right: 5px"
+          color="blue"
+          text="Modifica"
+          @click="dialog = true; emptySpaces(); paymentModify=true"
+        ></v-btn> 
+        <v-btn
+          outlined
+          width="150"
+          color="red"
+          text="Elimina"
+          @click="deleteDialog = true;"
+        ></v-btn> 
+      </v-card-item>
 
-        <v-card-item>
-          <v-btn          
-            color="yellow-darken-4" 
-            @click="dialog = true; ">
-            Aggiungi
-          </v-btn>
-        </v-card-item>
-
-        <v-dialog
-          persistent
-          v-model="dialog"
-          width="500">
+      <v-dialog
+        persistent
+        v-model="deleteDialog"
+        width="450">
           <v-card>
             <v-card-item>
               <div class="text-overline mb-1">
-                Inserimento metodo di pagamento
+                Conferma eliminazione metodo di pagamento
               </div>
             </v-card-item>
             <v-divider></v-divider>
-            <v-card-text class="pa-5">
-              <v-form ref="form">
-                <div class="font-weight-medium label-div">Numero carta</div>
-                  <v-text-field 
-                    placeholder="xxxx-xxxx-xxxx-xxxx"
-                    density="compact"
-                    :rules="cardNumberRules"
-                    :value="formatCardNumber"
-                    @input="updateValue"
-                    v-model="valueFields.cardNumber" 
-                    @keypress="isNumber($event)"
-                    maxlength="19"
-                    variant="outlined">
-                  </v-text-field>
-                  <div class="font-weight-medium label-div">Titolare carta</div>
-                  <v-text-field 
-                    placeholder="es. Mario Rossi"
-                    density="compact"
-                    :rules="cardNameRules"
-                    v-model="valueFields.cardName"
-                    variant="outlined">
-                  </v-text-field>
-                  <v-row style="margin-bottom: 5px">
-                    <v-col cols="8">
-                      <div class="font-weight-medium label-div">Scadenza carta</div>
-                      <div style="display: flex">
-                        <v-select
-                          label="Mese"
-                          style="margin-right: 15px"
-                          density="compact"
-                          variant="outlined"
-                          v-model="valueFields.cardMonth"
-                          :items="months"
-                          :rules="dateYearRules"></v-select>
-                        <v-select 
-                          label="Anno"
-                          density="compact"
-                          variant="outlined"
-                          v-model="valueFields.cardYear"
-                          :items="years"
-                          :rules="dateYearRules"></v-select>
-                      </div>
-                    </v-col>
-                    <v-col>
-                      <div class="font-weight-medium label-div">CVV/CVC</div>
-                      <v-text-field 
-                        @keypress="isNumber($event)"
-                        maxlength="3"
-                        placeholder="xxx"
-                        density="compact"
-                        variant="outlined"
-                        v-model="valueFields.cardCvv"
-                        :rules="cardCvvRules"></v-text-field>
-                    </v-col>
-                  </v-row>
+            <v-card-text class="font-weight-regular">
+              Vuoi confermare l'eliminazione del tuo metodo di pagamento?
+            </v-card-text>
+            <v-card-item>
+              <v-row >
+                <v-col align-self="end">
                   <v-btn
+                    text="Conferma"
                     outlined 
                     color="green"
-                    block
-                    class="me-4"
-                    text="Conferma"
-                    @click="validate"
-                    style="margin-bottom: 10px"
-                  ></v-btn>
+                    @click="deletePayment"
+                    style="margin-bottom: 10px; margin-right: 5px">                 
+                  </v-btn>
                   <v-btn
+                    text="Cancella"
                     outlined
                     color="red"
-                    block
-                    @click="cancelPayInsert"
-                    text="Annulla"
-                  ></v-btn>
-              </v-form>
-            </v-card-text>
+                    @click="deleteDialog = false"
+                    style="margin-bottom: 10px">
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-item>
           </v-card>
-        </v-dialog>
-      </div>
+      </v-dialog>
 
-      <div class="paycard" v-else>
-        <v-card-item>
-        <vue-paycard :value-fields="valueFields"
-            :has-random-backgrounds="false"
-            style="margin-bottom:15px"/>
-        </v-card-item>
-        <v-card-item>
-          <v-btn
-            outlined
-            width="150"
-            style="margin-right: 5px"
-            color="blue"
-            text="Modifica"
-          ></v-btn> 
-          <v-btn
-            outlined
-            width="150"
-            color="red"
-            text="Elimina"
-            @click="deleteDialog = true;"
-          ></v-btn> 
-        </v-card-item>
-
-        <v-dialog
-          persistent
-          v-model="deleteDialog"
-          width="450">
-            <v-card>
-              <v-card-item>
-                <div class="text-overline mb-1">
-                  Conferma eliminazione metodo di pagamento
-                </div>
-              </v-card-item>
-              <v-divider></v-divider>
-              <v-card-text class="font-weight-regular">
-                Vuoi confermare l'eliminazione del tuo metodo di pagamento?
-              </v-card-text>
-              <v-card-item>
-                <v-row >
-                  <v-col align-self="end">
-                    <v-btn
-                      text="Conferma"
-                      outlined 
-                      color="green"
-                      @click="deletePayment"
-                      style="margin-bottom: 10px; margin-right: 5px">                 
-                    </v-btn>
-                    <v-btn
-                      text="Cancella"
-                      outlined
-                      color="red"
-                      @click="deleteDialog = false"
-                      style="margin-bottom: 10px">
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-card-item>
-            </v-card>
-        </v-dialog>
-      </div>
-    </v-card>
+    </div>
+  </v-card>
 </template>
 
 <script>
@@ -196,8 +198,6 @@ export default {
       this.valueFields.cardYear = exp_date.split("/")[1]
 
       this.valueFields.cardCvv = this.$store.getters.userPaymentMethod.card_cvv.toString()
-    } else {
-      console.log("dsds")
     }
   },
   data: () => ({
@@ -208,6 +208,7 @@ export default {
       cardYear: "",
       cardCvv: "",
     },
+    paymentModify: false,
     deleteDialog: false,
     dialog: false,
     months: [],
@@ -243,7 +244,12 @@ export default {
   methods: {
     cancelPayInsert() {
       this.dialog = false
-      this.$refs.form.reset()
+      if (this.paymentModify == false) {
+        this.$refs.form.reset()
+      }
+      this.payment_method = false
+      let number = this.$store.getters.userPaymentMethod.card_number.toString()
+      this.valueFields.cardNumber = number.match(/.{1,4}/g).join(" ")
     },
     updateValue(e){
       this.valueFields.cardNumber = e.target.value.replace(/ /g,'');
@@ -278,6 +284,7 @@ export default {
 
           this.$store.dispatch('user/paymentMethodInsert', data)
           this.dialog = false
+          this.payment_method = false
           this.$refs.form.submit();
         }
     },
@@ -288,6 +295,10 @@ export default {
       this.$store.dispatch('user/deletePaymentMethod', user_email)
       location.reload()
       this.deleteDialog = false
+    },
+    emptySpaces() {
+      this.valueFields.cardNumber = this.valueFields.cardNumber.replace(/\s/g, '');
+      console.log(this.valueFields.cardNumber)
     }
   }
 
